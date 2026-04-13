59a1f7b1e18547c6f7e2041b10281f800b7c05cd
gillerick <ogayogill95@gmail.com>
2026-04-13 12:13:46 +0300

    refactor(KK-12118): Rename pay recipient to external recipient

--- spec/k2_tests/service_tests/send_k2_connect_post_request_service_spec.rb
+++ spec/k2_tests/service_tests/send_k2_connect_post_request_service_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe K2ConnectRuby::K2Services::SendK2ConnectPostRequestService do
   describe '#call' do
     context 'when you pass invalid credentials' do
       it 'should raise n k2 connect unauthorized error' do
-        pay_recipient_url = 'some-url'
-        stub_request(:post, pay_recipient_url)
+        external_recipient_url = 'some-url'
+        stub_request(:post, external_recipient_url)
           .to_return(body: { access_token: 'access_token' }.to_json, status: 401)
         expect do
           K2ConnectRuby::K2Services::SendK2ConnectPostRequestService.call(
             'access_token',
-            pay_recipient_url,
+            external_recipient_url,
             {}
           )
         end.to raise_error K2ConnectRuby::K2Errors::UnauthorizedError
@@ -19,12 +19,12 @@ RSpec.describe K2ConnectRuby::K2Services::SendK2ConnectPostRequestService do
 
     context 'when the api has a timeout' do
       it 'should raise a k2 connect timeout error' do
-        pay_recipient_url = 'some-url'
-        stub_request(:post, pay_recipient_url).to_timeout
+        external_recipient_url = 'some-url'
+        stub_request(:post, external_recipient_url).to_timeout
         expect do
           K2ConnectRuby::K2Services::SendK2ConnectPostRequestService.call(
             'access_token',
-            pay_recipient_url,
+            external_recipient_url,
             {}
           )
         end.to raise_error K2ConnectRuby::K2Errors::TimeoutError
@@ -33,12 +33,12 @@ RSpec.describe K2ConnectRuby::K2Services::SendK2ConnectPostRequestService do
 
     context 'when the api has a connection refused' do
       it 'should raise a k2 connect connection refused' do
-        pay_recipient_url = 'some-url'
-        stub_request(:post, pay_recipient_url).to_raise(Errno::ECONNREFUSED)
+        external_recipient_url = 'some-url'
+        stub_request(:post, external_recipient_url).to_raise(Errno::ECONNREFUSED)
         expect do
           K2ConnectRuby::K2Services::SendK2ConnectPostRequestService.call(
             'access_token',
-            pay_recipient_url,
+            external_recipient_url,
             {}
           )
         end.to raise_error K2ConnectRuby::K2Errors::ConnectionError
@@ -47,26 +47,26 @@ RSpec.describe K2ConnectRuby::K2Services::SendK2ConnectPostRequestService do
 
     context 'when the api has a uncaught error' do
       it 'should raise a generic k2 connect api error' do
-        pay_recipient_url = 'some-url'
-        stub_request(:post, pay_recipient_url).to_return(status: 500,
+        external_recipient_url = 'some-url'
+        stub_request(:post, external_recipient_url).to_return(status: 500,
                                                          body: { error_message: 'internal server error' }.to_json)
         expect do
           K2ConnectRuby::K2Services::SendK2ConnectPostRequestService.call(
             'access_token',
-            pay_recipient_url,
+            external_recipient_url,
             {}
           )
         end.to raise_error K2ConnectRuby::K2Errors::ApiError
       end
 
       it 'should raise with the correct error message' do
-        pay_recipient_url = 'some-url'
-        stub_request(:post, pay_recipient_url).to_return(status: 500,
+        external_recipient_url = 'some-url'
+        stub_request(:post, external_recipient_url).to_return(status: 500,
                                                          body: { error_message: 'internal server error' }.to_json)
         begin
           K2ConnectRuby::K2Services::SendK2ConnectPostRequestService.call(
             'access_token',
-            pay_recipient_url,
+            external_recipient_url,
             {}
           )
         rescue K2ConnectRuby::K2Errors::ApiError => e
@@ -75,13 +75,13 @@ RSpec.describe K2ConnectRuby::K2Services::SendK2ConnectPostRequestService do
       end
 
       it 'exposes the API error message details' do
-        pay_recipient_url = 'some-url'
-        stub_request(:post, pay_recipient_url).to_return(status: 500,
+        external_recipient_url = 'some-url'
+        stub_request(:post, external_recipient_url).to_return(status: 500,
                                                          body: { error_message: 'internal server error' }.to_json)
         begin
           K2ConnectRuby::K2Services::SendK2ConnectPostRequestService.call(
             'access_token',
-            pay_recipient_url,
+            external_recipient_url,
             {}
           )
         rescue K2ConnectRuby::K2Errors::ApiError => e
